e0e6bf98e241346212bda38a0717a92618d4ad0b
T-RICH-92 <trichar2@harding.edu>
2015-06-29 22:57:11 -0400

    Initial Project setup and project 1

---
+++ src/com/richstudios/evilgenius/section1/project1/Repeater.java
@@ -0,0 +1,13 @@
+package com.richstudios.evilgenius.section1.project1;
+
+/**
+ * Created by T RICH on 6/29/2015.
+ */
+public class Repeater {
+    public static void main (String[] args) {
+        System.out.println("Your input:");
+        for(String s : args) {
+            System.out.println(s);
+        }
+    }
+}
